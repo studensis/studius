01651681f828aecece9d0b3918f9452faeb44255
Matija Fućek <matijafucek1@gmail.com>
2022-12-20 01:27:05 +0100

    fixed getUserById functionality

--- backend/src/domain/User/routeHandlers/getUserRouteHandler.ts
+++ backend/src/domain/User/routeHandlers/getUserRouteHandler.ts
@@ -7,7 +7,7 @@ export default async function getUserRouteHandler(req: Request, res: Response) {
 	console.log('/users/:userId GET');
 
 	try {
-		let id = Number(req.params.userId);
+		let id = req.params.userId;
 		let repo = new UserRepositoryPrisma();
 		let user = await getUserInteractor(repo, id);
 		return res.send(user);
