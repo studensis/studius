fc4510cd2542f12a40c2a49af7c26339bee9fecb
Matija Fućek <matijafucek1@gmail.com>
2023-01-07 19:07:44 +0100

    logout bugfix

--- backend/src/controllers/Authentication/authRouter/logoutRoutine.ts
+++ backend/src/controllers/Authentication/authRouter/logoutRoutine.ts
@@ -1,6 +1,16 @@
 import { t } from '../../trpc';
 
 export default t.procedure.mutation(({ ctx }) => {
-	ctx.res.clearCookie('token');
+	// ctx.res.clearCookie('token');
+	ctx.res.cookie('token', 'deleted', {
+		...(process.env.NODE_ENV === 'production' && {
+			sameSite: 'none',
+			secure: true,
+		}),
+		maxAge: 1000 * 60 * 60, // 1h
+		// httpOnly: true,
+		// secure: true,
+		// signed: true
+	});
 	return 'logged out successfully';
 });
