922e81bb2f09eae784b694492fbb38fe33dca926
Matija Fućek <matijafucek1@gmail.com>
2023-01-09 21:57:55 +0100

    mock event on calendar page

--- frontend/app/(nonLogin)/calendar/page.tsx
+++ frontend/app/(nonLogin)/calendar/page.tsx
@@ -1,14 +1,38 @@
 'use client';
 
-import { PageStack } from '../../../components/@studius/PageElements/Stack';
+import Link from 'next/link';
+import { Block } from '../../../components/@studius/PageElements/Block';
+import {
+	PageStack,
+	Stack,
+} from '../../../components/@studius/PageElements/Stack';
 
 export default function Calendar() {
 	return (
 		<>
 			<PageStack>
-				<h1 className="display3">Events</h1>
 				<h1 className="title1">Calendar</h1>
-				<h1 className="title1">Event list</h1>
+				<Block>
+					<div className="h-40"></div>
+				</Block>
+				<h2 className="title1">Today</h2>
+				<Stack cols={2}>
+					<Link href={'/event/1'}>
+						<Block>
+							<p className="title2">23. Predavanje</p>
+						</Block>
+					</Link>
+					<Link href={'/event/1'}>
+						<Block>
+							<p className="title2">23. Predavanje</p>
+						</Block>
+					</Link>
+					<Link href={'/event/1'}>
+						<Block>
+							<p className="title2">23. Predavanje</p>
+						</Block>
+					</Link>
+				</Stack>
 			</PageStack>
 		</>
 	);
