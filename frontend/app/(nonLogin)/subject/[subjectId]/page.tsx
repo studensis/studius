8453b5f5575c6634d0d4c8dab252cb51e0690573
Adrian <adriananicic@gmail.com>
2023-01-10 14:45:40 +0100

    dodan Add Students to subject

--- frontend/app/(nonLogin)/subject/[subjectId]/page.tsx
+++ frontend/app/(nonLogin)/subject/[subjectId]/page.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
 import { Button } from '../../../../components/@studius/Button/Button';
 import { SectionTop } from '../../../../components/@studius/PageElements/SectionTop';
 import {
@@ -8,6 +10,7 @@ import {
 } from '../../../../components/@studius/PageElements/Stack';
 import PageHeader from '../../../../components/@studius/PageHeader/PageHeader';
 import UserCard from '../../../../components/Cards/UserCard';
+import EnrollStudentsSection from '../../../../components/EnrollStudentsSection';
 import { trpc } from '../../../../components/hooks/TrpcProvider';
 
 type PageProps = {
@@ -18,11 +21,15 @@ type PageProps = {
 
 function SubjectPage(props: PageProps) {
 	const subject = trpc.subject.getSubjectById.useQuery(props.params.subjectId);
+
 	const enrolledUsers = trpc.subject.getEnrolledUsers.useQuery(
 		props.params.subjectId
 	);
+	const router = useRouter();
 
 	const enroll = trpc.user.updateEnrollment.useMutation();
+
+	const [enrollmentPage, setEnrollmentPage] = useState(false);
 
 	function unenroll(id: string) {
 		enroll.mutate({
@@ -30,6 +37,9 @@ function SubjectPage(props: PageProps) {
 			subjectId: props.params.subjectId,
 			status: 'ARCHIVED',
 		});
+		//Iz nekog razloga refresh ne radi, nego da vidis promjenu moras promjenit tab pa se vratit.
+		router.refresh();
+		console.log('hary');
 	}
 
 	return (
@@ -41,8 +51,25 @@ function SubjectPage(props: PageProps) {
 				/>
 
 				<div>
+					<Button
+						onClick={() => {
+							setEnrollmentPage(!enrollmentPage);
+						}}
+						className="m-5 mb-8"
+					>
+						Enroll users
+					</Button>
+
+					{enrollmentPage && (
+						<EnrollStudentsSection
+							enrollmentPage={enrollmentPage}
+							setEnrollmentPage={setEnrollmentPage}
+							subjectId={props.params.subjectId}
+						/>
+					)}
+
 					<SectionTop>
-						<h3 className="title2">Enrolled Users</h3>
+						<h3 className="title2">List of enrolled Users:</h3>
 					</SectionTop>
 					<Stack cols={2}>
 						{enrolledUsers.data &&
--- frontend/components/Cards/UserCard.tsx
+++ frontend/components/Cards/UserCard.tsx
@@ -6,12 +6,18 @@ const UserCard: FC<{
 	user: { id: string; firstname: string; lastname: string };
 }> = ({ user }) => {
 	return (
-		<Link href={'/user/' + user.id} key={user.id}>
-			<Block>
-				<p className="title1">{user.firstname + ' ' + user.lastname}</p>
-				<p className="text-neutral-strong caption">{user.id}</p>
-			</Block>
-		</Link>
+		<div className="">
+			<Link
+				href={'/user/' + user.id}
+				key={user.id}
+				className="border-accent-medium"
+			>
+				<Block>
+					<p className="title1">{user.firstname + ' ' + user.lastname}</p>
+					<p className="text-neutral-strong caption">{user.id}</p>
+				</Block>
+			</Link>
+		</div>
 	);
 };
 
