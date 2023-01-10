431f9722e4a9430695bc787026c0b16ce39bb8e9
Matija Fućek <matijafucek1@gmail.com>
2023-01-10 17:29:07 +0100

    fixing type errors

--- backend/src/controllers/router.ts
+++ backend/src/controllers/router.ts
@@ -1,10 +1,7 @@
 import contentRouter from '../domain/Content/contentRouter';
 import eventRouter from '../domain/Event/eventRouter';
-import eventUserPresenceRouter from '../domain/EventUserPresence/EventUserPresenceRouter';
 import postRouter from '../domain/Post/postRouter';
 import roomRouter from '../domain/Room/roomRouter';
-import roomTimeEventRouter from '../domain/RoomTimeEvent/roomTimeEventRouter';
-import seminarSuggestionRouter from '../domain/SeminarSuggestion/seminarSuggestionRouter';
 import subjectRouter from '../domain/Subject/subjectRouter';
 import userRouter from '../domain/User/userRouter';
 import authRouter from '../services/authentication/authRouter';
@@ -15,18 +12,15 @@ const todoRoutine = t.procedure.query(() => {
 });
 
 // root router to call
-const appRouter: any = t.router({
+const appRouter = t.router({
 	auth: authRouter,
 	user: userRouter,
 	subject: subjectRouter,
 	// enrollment: todoRoutine,
 	room: roomRouter,
 	event: eventRouter,
-	roomTimeEvent: roomTimeEventRouter,
-	eventUserPresence: eventUserPresenceRouter,
 	post: postRouter,
 	seminar: todoRoutine,
-	seminarSuggestion: seminarSuggestionRouter,
 	content: contentRouter,
 });
 
