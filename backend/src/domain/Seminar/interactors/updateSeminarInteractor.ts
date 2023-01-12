62cd6e152a9e895e292e923f7499e5d7c45af599
Hary S <hary.samardzic@gmail.com>
2023-01-12 17:42:26 +0100

    dodana funkcionalnost da user ne moze updateati seminar ako je on vec CONFIRMED, dodano READY u enum SeminarStatus

--- backend/src/domain/Seminar/interactors/updateSeminarInteractor.ts
+++ backend/src/domain/Seminar/interactors/updateSeminarInteractor.ts
@@ -1,3 +1,4 @@
+import { TRPCError } from '@trpc/server';
 import { updateSeminarEntity } from '../model/updateSeminarEntity';
 import { SeminarRepository } from '../repository/SeminarRepository';
 
@@ -5,6 +6,13 @@ export default async function updateSeminarInteractor(
 	seminarRepository: SeminarRepository,
 	seminar: updateSeminarEntity
 ) {
+	let existingSeminar = await seminarRepository.getById(seminar.id);
+	if (existingSeminar.status == 'CONFIRMED') {
+		throw new TRPCError({
+			code: 'BAD_REQUEST',
+			message: 'Unable to update',
+		});
+	}
 	let updatedSeminar = await seminarRepository.update(seminar);
 	return updatedSeminar;
 }
