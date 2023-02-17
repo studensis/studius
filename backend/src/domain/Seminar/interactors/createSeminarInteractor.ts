54a0622f74d8bbe532b9cf796a63059bf634199f
Hary S <hary.samardzic@gmail.com>
2023-02-17 16:08:57 +0100

    dodana provjera podataka za kreiranje seminara

--- backend/src/domain/Seminar/interactors/createSeminarInteractor.ts
+++ backend/src/domain/Seminar/interactors/createSeminarInteractor.ts
@@ -1,3 +1,4 @@
+import { TRPCError } from '@trpc/server';
 import { SeminarEntity } from '../model/SeminarEntity';
 import { SeminarRepository } from '../repository/SeminarRepository';
 
@@ -5,6 +6,18 @@ export default async function createSeminarInteractor(
 	seminarRepository: SeminarRepository,
 	seminar: SeminarEntity
 ) {
+	if (
+		!seminar.title ||
+		!seminar.description ||
+		!seminar.mentorId ||
+		!seminar.userId ||
+		!seminar.subjectId
+	) {
+		throw new TRPCError({
+			code: 'BAD_REQUEST',
+			message: 'Nepotpuni podaci',
+		});
+	}
 	let seminars = await seminarRepository.create(seminar);
 	return seminars;
 }
