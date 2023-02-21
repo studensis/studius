c05de7d6e0b83392b04df5aa81957ce7a714ef5b
Vid Mulc <vidmulc@gmail.com>
2023-02-21 17:15:24 +0100

    Error 404 Page

--- frontend/app/not-found.tsx
+++ frontend/app/not-found.tsx
@@ -1,3 +1,27 @@
+import Header from '../components/Header/Header';
+
 export default function PostNotFound() {
-	return <p>Uh oh! This post could not be found.</p>;
+	return (
+		<>
+			<Header />
+			<div className="grid w-screen h-full place-items-center">
+				<div className="absolute  center">
+					<p className="display1 z-10">404</p>
+					<p className="display3 -ml-14">Page not found</p>
+				</div>
+				<img
+					className="h-screen w-screen -z-10 hidden md:block"
+					style={{ objectFit: 'cover' }}
+					src="/assets/images/background_desktop.svg"
+					alt="Image not found."
+				/>
+				<img
+					className="h-screen w-screen -z-10 block md:hidden"
+					style={{ objectFit: 'cover' }}
+					src="/assets/images/background_mobile.svg"
+					alt="Image not found."
+				/>
+			</div>
+		</>
+	);
 }
