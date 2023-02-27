4dbefd3eb19951574b3b63b1dd33ec1f68e8458d
Vid Mulc <vidmulc@gmail.com>
2023-02-27 09:45:27 +0100

    Minor loading animation bug fixes.

--- frontend/app/(nonLogin)/intranet/loadingCalendar.tsx
+++ frontend/app/(nonLogin)/intranet/loadingCalendar.tsx
@@ -1,7 +1,7 @@
 export default function LoadingCalendar() {
 	// You can add any UI inside Loading, including a Skeleton.
 	return (
-		<div className=" min-w-full flex flex-col md:px-10 sm:px-6 py-10 bg-section rounded-xl">
+		<div className=" min-w-full flex flex-col md:px-10 sm:px-6 py-10 bg-section rounded-xl overflow-x-auto">
 			<div className="flex justify-between mb-2 min-h-[40px]">
 				<div className="rounded-md min-w-[100px] flex-col animated-background"></div>
 				<div className="flex gap-0">
@@ -10,11 +10,11 @@ export default function LoadingCalendar() {
 				</div>
 			</div>
 			<div>
-				<div className="flex w-full mx-3 justify-between">
+				<div className="flex flex-row w-full justify-between ">
 					{[...new Array(7)].map(() => (
 						<div className="flex flex-col">
 							{[...new Array(6)].map(() => (
-								<div className="flex animated-background flex-1 text-center min-w-[32px] min-h-[32px] rounded-xl mr-[13.72px] mb-[8px] "></div>
+								<div className="flex flex-1 animated-background text-center min-w-[32px] min-h-[32px] rounded-xl mr-[13.72px] mb-[8px] "></div>
 							))}
 						</div>
 					))}
